95e081644bb45e33dd551be89fe54be908a8ee3c
arfin-arif <arifmtbd@gmail.com>
2022-12-30 19:03:23 +0600

    dashboard added

--- components/Navbar/HeaderItem.tsx
+++ components/Navbar/HeaderItem.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface Props {
   title: string;
   Icon: any;
@@ -5,13 +7,13 @@ interface Props {
 
 // #1670D1
 
-const HeaderItem = ({ title, Icon }: Props) => {
+const HeaderItem = ({ title, Icon, url }: Props) => {
   return (
     <div className="group  flex flex-col items-center hover:cursor-pointer w-12 md:w-20 ">
-      <Icon className="h-8 mb-1 group-hover:text-slate-900 text-[#1670D1] group-hover:animate-bounce" />
+      <Link href={`${url}`}><Icon className="h-8 mb-1 group-hover:text-slate-900 text-[#1670D1] group-hover:animate-bounce" />
+      </Link>
       <p className="tracking-widest group-hover:text-[#1670D1] opacity-0 group-hover:opacity-100">
-        {title}
-      </p>
+        {title}</p>
     </div>
   );
 };
