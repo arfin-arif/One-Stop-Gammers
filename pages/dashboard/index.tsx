2b931e19301e659d8ef09db8adf058664eaf6220
jahidhasan8 <jahidhasan1932@gmail.com>
2023-01-02 11:47:28 +0600

    dashboard modified

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -27,7 +27,15 @@ const Dashboard = () => {
                             <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
 
-                        <span className="mx-4 font-medium">Accounts</span>
+                        <span className="mx-4 font-medium">
+
+                        <select className="select w-full max-w-xs">
+                        <option disabled selected>Top Up</option>
+                        <option>Free Fire</option>
+                         <option>Pubg</option>
+                           <option>Clash Of Clans</option>
+                        </select>
+                        </span>
                     </a>
 
                     <a className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
@@ -35,7 +43,14 @@ const Dashboard = () => {
                             <path d="M15 5V7M15 11V13M15 17V19M5 5C3.89543 5 3 5.89543 3 7V10C4.10457 10 5 10.8954 5 12C5 13.1046 4.10457 14 3 14V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V14C19.8954 14 19 13.1046 19 12C19 10.8954 19.8954 10 21 10V7C21 5.89543 20.1046 5 19 5H5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
 
-                        <span className="mx-4 font-medium">Tickets</span>
+                        <span className="mx-4 font-medium">
+                        <select className="select w-full max-w-xs">
+                        <option disabled selected>Gift Card</option>
+                        <option>Amazon</option>
+                         <option>Google</option>
+                           <option>Itunes</option>
+                        </select>
+                        </span>
                     </a>
 
                     <a className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
